98b0e008924ee9d428dd444cca3eba8446571ae2
sarsing <sardar_singh@gap.com>
2022-08-23 22:51:49 +0530

    Initial commit

---
+++ src/main/java/com/k8/challenge/kn8sshield/Kn8sShieldApplication.java
@@ -0,0 +1,13 @@
+package com.k8.challenge.kn8sshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kn8sShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kn8sShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8/challenge/kn8sshield/Kn8sShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8.challenge.kn8sshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kn8sShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
